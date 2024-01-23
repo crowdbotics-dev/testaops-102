475194f06713cd6f355341da65aa131d486f5ac5
ravinder yadav <ravinder.y@crowdbotics.com>
2024-01-23 11:10:09 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/testaopsAPI/api.js
+++ store/testaopsAPI/api.js
@@ -1,52 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const testaopsAPI = axios.create({
   baseURL: "https://testaops-102.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return testaopsAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return testaopsAPI.post(`/api/v1/login/`, payload)
+  return testaopsAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return testaopsAPI.post(`/api/v1/signup/`, payload)
+  return testaopsAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return testaopsAPI.post(`/rest-auth/login/`, payload)
+  return testaopsAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_create(payload) {
-  return testaopsAPI.post(`/rest-auth/logout/`)
+  return testaopsAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return testaopsAPI.post(`/rest-auth/password/change/`, payload)
+  return testaopsAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return testaopsAPI.post(`/rest-auth/password/reset/`, payload)
+  return testaopsAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return testaopsAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return testaopsAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return testaopsAPI.post(`/rest-auth/registration/`, payload)
+  return testaopsAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_resend_email_create(payload) {
-  return testaopsAPI.post(`/rest-auth/registration/resend-email/`, payload)
+  return testaopsAPI.post(`/rest-auth/registration/resend-email/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return testaopsAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return testaopsAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return testaopsAPI.get(`/rest-auth/user/`)
+  return testaopsAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return testaopsAPI.put(`/rest-auth/user/`, payload)
+  return testaopsAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return testaopsAPI.patch(`/rest-auth/user/`, payload)
+  return testaopsAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -62,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
